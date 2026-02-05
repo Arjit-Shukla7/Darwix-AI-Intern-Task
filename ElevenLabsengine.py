ab3ef3719d6f4358b24460ed0adad55c548b278b
Arjit-Shukla7 <krishnashuks2003@gmail.com>
2026-02-06 02:15:27 +0530

    Added the voice setting in the main audio stream function

--- ElevenLabsengine.py
+++ ElevenLabsengine.py
@@ -1,3 +1,4 @@
+from click import style
 import os
 from dotenv import load_dotenv
 from elevenlabs.play import play
@@ -14,6 +15,13 @@ def bolnegai11(text,params):
             text=text,
             voice_id="JBFqnCBsd6RMkjVDRZzb",
             model_id="eleven_multilingual_v2",
+            voice_settings=VoiceSettings(
+                stability=params['stability'],
+                similarity_boost=params['similarity_boost'],
+                style=params['style'],
+                use_speaker_boost=params['use_speaker_boost'],
+                speed=params['speed']
+            )
         )
         print("Speaking....")
         play(audio_stream)
@@ -30,9 +38,9 @@ def bolnegai11(text,params):
 
 if __name__ == "__main__":
     test_params = {
-        "stability": 0.35,
-        "similarity_boost": 0.75, 
-        "style": 0.6, 
+        "stability": 0.3,
+        "similarity_boost": 0.3, 
+        "style": 0.3, 
         "use_speaker_boost": True, 
         "speed": 1.15
     }
